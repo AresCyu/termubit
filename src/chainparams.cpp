038cc79aee75ca286aa1c7cfb5274a6ab003c303
aldianokto <aldianokto@gmail.com>
2026-05-01 13:16:24 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
-// Copyright (c) 2022-2024 The Termubit Core developers
+// Copyright (c) 2026 The Termubit Core developers
+// Copyright (c) 2026-2028 Aldianokto
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
