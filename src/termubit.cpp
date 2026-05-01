272731756232496c93accfe5d460163095778119
aldianokto <aldianokto@gmail.com>
2026-05-01 14:24:21 +0700

    Update termubit.cpp

--- src/termubit.cpp
+++ src/termubit.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2026 The Termubit Core developers
+// Copyright (c) 2026-2028 Aldianokto
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
