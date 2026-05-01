c390b7b402f1b0ce1d2b5fff435cd79bcb740d8b
aldianokto <aldianokto@gmail.com>
2026-05-01 13:26:31 +0700

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -1,4 +1,6 @@
 // Copyright (c) 2012-2016 The Bitcoin Core developers
+// Copyright (c) 2026 The Termubit Core developers
+// Copyright (c) 2026-2028 Aldianokto
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
