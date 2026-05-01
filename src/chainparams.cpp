3e08f6d46765265d92bba80245a57262d1e21847
aldianokto <aldianokto@gmail.com>
2026-05-01 23:33:11 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,7 +122,7 @@ public:
         consensus.fAllowLegacyBlocks = true;
         consensus.nHeightEffective = 0;
 
-        // Blocks 1420 - 1420 are Digishield without AuxPoW
+        // Blocks 1420 - 2100 are Digishield without AuxPoW
         digishieldConsensus = consensus;
         digishieldConsensus.nHeightEffective = 1420;
         digishieldConsensus.fSimplifiedRewards = true;
@@ -130,9 +130,9 @@ public:
         digishieldConsensus.nPowTargetTimespan = 60; // post-digishield: 1 minute
         digishieldConsensus.nCoinbaseMaturity = 240;
 
-        // Blocks 1420+ are AuxPoW
+        // Blocks 2100+ are AuxPoW
         auxpowConsensus = digishieldConsensus;
-        auxpowConsensus.nHeightEffective = 1420;
+        auxpowConsensus.nHeightEffective = 2100;
         auxpowConsensus.fAllowLegacyBlocks = false;
 
         // Assemble the binary search tree of consensus parameters
