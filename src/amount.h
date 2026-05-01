c6705c3d4065094d445d4901019b3dc2eab9d9bc
aldianokto <aldianokto@gmail.com>
2026-05-01 22:02:00 +0700

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -29,7 +29,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 281000000 * COIN; // Termubit: maximum of 281000000 coins (given some randomness), max transaction 281000000
+static const CAmount MAX_MONEY = 281000000 * COIN; // Termubit: maximum of 281.000.000 coins (given some randomness), max transaction 281.000.000
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
